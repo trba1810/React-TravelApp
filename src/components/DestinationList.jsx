ac1a420ff75baa1a3fed35aae7b4fac5a5d70863
trba1810 <trba8l@live.com>
2023-02-07 23:14:53 +0100

    separate destination component

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,49 +1,27 @@
 import React from "react";
-import {
-  useGetAllDestinationQuery,
-  useDeleteDestinationMutation,
-} from "../api/DestinationAPI";
+import { useGetAllDestinationQuery } from "../api/DestinationAPI";
+import Destination from "./Destination";
 
 function DestinationList() {
   const { data, isLoading, isSuccess, isError, error } =
     useGetAllDestinationQuery();
   let content;
-  const [deleteDestination] = useDeleteDestinationMutation();
 
   if (isLoading) {
     content = <p>Loading...</p>;
   } else if (isSuccess) {
     content = data.map((destination) => {
       return (
-        <div
-          className="row py-1"
+        <Destination
+          destination={destination}
           key={destination.id}
-          style={{
-            borderBottom: "1px solid #333",
-            borderTop: "1px solid #333",
-          }}
-        >
-          <div className="col-3 offset-3">
-            {destination.city},{destination.country}
-          </div>
-          <div className="col-1 text-warning">
-            {destination.daysNeeded} days
-          </div>
-          <div className="col-2">
-            <button
-              className="btn form-control btn-danger"
-              onClick={() => deleteDestination({ id: destination.id })}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
+        ></Destination>
       );
     });
   } else if (isError) {
     content = <p>{error}</p>;
   }
-  return <div>{content}</div>;
+  return <div className="pt-3">{content}</div>;
 }
 
 export default DestinationList;
