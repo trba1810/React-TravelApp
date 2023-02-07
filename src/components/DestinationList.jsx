3f2980e5861346df40b958229badefe5a1bc7633
trba1810 <trba8l@live.com>
2023-02-07 17:27:03 +0100

    added delete button

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,22 +1,43 @@
 import React from "react";
-import { useGetAllDestinationQuery } from "../api/DestinationAPI";
+import {
+  useGetAllDestinationQuery,
+  useDeleteDestinationMutation,
+} from "../api/DestinationAPI";
 
 function DestinationList() {
   const { data, isLoading, isSuccess, isError, error } =
     useGetAllDestinationQuery();
   let content;
+  const [deleteDestination] = useDeleteDestinationMutation();
+
   if (isLoading) {
     content = <p>Loading...</p>;
   } else if (isSuccess) {
     content = data.map((destination) => {
       return (
-        <article key={destination.id}>
-          <div className="text-center text-info p-2">
-            <div>
-              {destination.city},{destination.country}-{destination.daysNeeded}
-            </div>
+        <div
+          className="row py-1"
+          key={destination.id}
+          style={{
+            borderBottom: "1px solid #333",
+            borderTop: "1px solid #333",
+          }}
+        >
+          <div className="col-3 offset-3">
+            {destination.city},{destination.country}
           </div>
-        </article>
+          <div className="col-1 text-warning">
+            {destination.daysNeeded} days
+          </div>
+          <div className="col-2">
+            <button
+              className="btn form-control btn-danger"
+              onClick={() => deleteDestination({ id: destination.id })}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
       );
     });
   } else if (isError) {
