797770d044b7ffd1aa3d3e5313ce31ba0e0ac60d
trba1810 <trba8l@live.com>
2023-02-08 15:29:08 +0100

    add another api to project

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { DestinationAPI } from "../api/DestinationAPI";
+import { RandomDestinationAPI } from "../api/RandomDestinationApi";
 
 export const store = configureStore({
-  reducer: { [DestinationAPI.reducerPath]: DestinationAPI.reducer },
+  reducer: {
+    [DestinationAPI.reducerPath]: DestinationAPI.reducer,
+    [RandomDestinationAPI.reducerPath]: RandomDestinationAPI.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(DestinationAPI.middleware),
+    getDefaultMiddleware()
+      .concat(DestinationAPI.middleware)
+      .concat(RandomDestinationAPI.middleware),
 });
